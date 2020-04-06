a6952123a85f9726bd9225addcbaf92899fc8b69
DangChinh <dangchinh2501@gmail.com>
2020-04-06 02:31:38 -0500

    Add simple changing image function

--- src/components/SwipePages/InfoCard.js
+++ src/components/SwipePages/InfoCard.js
@@ -1,5 +1,7 @@
-import React from "react"
+import React, { useState } from "react"
 import styled from "styled-components"
+import ArrowForwardIosIcon from "@material-ui/icons/ArrowForwardIos"
+import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos"
 
 const CardContainer = styled.div`
 	position: relative;
@@ -19,6 +21,36 @@ const CardContainer = styled.div`
 		color: #fff;
 		width: 60%; /* Set the width of the positioned div */
 	}
+
+	.right {
+		position: absolute;
+		top: 50%;
+		right: 3px;
+		border-radius: 50%;
+		padding: 2px;
+		cursor: pointer;
+		box-shadow: 0px 0px 5px 2px rgba(0, 0, 0, 0.35);
+		transition: 0.2s all ease;
+
+		&:hover {
+			transform: scale(1.1);
+		}
+	}
+
+	.left {
+		position: absolute;
+		top: 50%;
+		left: 3px;
+		border-radius: 50%;
+		padding: 2px;
+		cursor: pointer;
+		box-shadow: 0px 0px 5px 2px rgba(0, 0, 0, 0.35);
+		transition: 0.2s all ease;
+
+		&:hover {
+			transform: scale(1.1);
+		}
+	}
 `
 
 const Image = styled.img`
@@ -26,9 +58,9 @@ const Image = styled.img`
 	height: 100%;
 	object-fit: cover;
 	border-radius: 1.5rem;
-	box-shadow: 0px 0px 9px 2px rgba(0, 0, 0, 0.35);
 	cursor: pointer;
 	transition: 0.2s all ease;
+	box-shadow: 0px 0px 5px 2px rgba(0, 0, 0, 0.35);
 
 	&:hover {
 		transform: scale(1.01);
@@ -36,9 +68,29 @@ const Image = styled.img`
 `
 
 function InfoCard({ name, age, desc, image }) {
+	const [currentImage, setCurrentImage] = useState(0)
+
+	const nextImage = () => {
+		if (currentImage === image.length - 1) {
+			setCurrentImage(0)
+			return
+		}
+		setCurrentImage(currentImage + 1)
+	}
+	const prevImage = () => {
+		if (currentImage <= 0) {
+			setCurrentImage(image.length - 1)
+			return
+		}
+		setCurrentImage(currentImage - 1)
+	}
+
 	return (
 		<CardContainer>
-			<Image src={`/images/users/${image}`} />
+			<Image src={`/images/users/${image[currentImage]}`} />
+			<ArrowBackIosIcon className="left" onClick={prevImage} />
+			<ArrowForwardIosIcon className="right" onClick={nextImage} />
+
 			<div className="info">
 				<p>{name}</p>
 				<p>{age}</p>
